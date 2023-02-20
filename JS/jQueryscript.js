12a21626dda4e7f7e8c18a436a677bf545dee037
valabilo <abilovalkrystoper@gmail.com>
2023-02-20 18:37:42 +0800

    first commit

---
+++ JS/jQueryscript.js
@@ -0,0 +1,24 @@
+
+    const swiperEl = document.querySelector('swiper-container')
+    Object.assign(swiperEl, {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+      },
+    });
+    swiperEl.initialize();
