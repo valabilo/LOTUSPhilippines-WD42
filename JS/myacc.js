25c3ba42aeecea1fb0facd8d661ea8403a6bd02f
valabilo <abilovalkrystoper@gmail.com>
2023-03-10 17:50:39 +0800

    updated version

---
+++ JS/myacc.js
@@ -0,0 +1,10 @@
+// Add active class to the current button (highlight it)
+var header = document.getElementById("myDIV");
+
+for (var i = 0; i < btns.length; i++) {
+  {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  };
+}
